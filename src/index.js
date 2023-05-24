8b6ec89650aea977dade12ded3aa7647b2474d7c
Valentin <gennoff@gmail.com>
2023-05-25 00:04:04 +0300

    Configuring the views and main.hbs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-const env = process.env.NODE_ENV || 'development';
-
-const config = require('./config/config')[env];
 const app = require('express')();
+const handlebars = require('express-handlebars');
+const port = 3000
 
-require('./config/express')(app);
-require('./config/routes')(app);
+// require('./config/express')(app);
+// require('./config/routes')(app);
 
-app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', 'src/views')
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+
+app.listen(port, () => console.log(`Listening on port ${port}! Now its up to you...`));
